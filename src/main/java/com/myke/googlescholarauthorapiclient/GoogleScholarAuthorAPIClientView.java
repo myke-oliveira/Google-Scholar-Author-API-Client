5c9a1e5d0e373f2efe01950d417dc82b8aabe672
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2023-04-20 17:44:04 -0300

    Update GoogleScholarAuthorAPIClientView.java

--- src/main/java/com/myke/googlescholarauthorapiclient/GoogleScholarAuthorAPIClientView.java
+++ src/main/java/com/myke/googlescholarauthorapiclient/GoogleScholarAuthorAPIClientView.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.myke.googlescholarauthorapiclient;
 
 import java.util.Scanner;
 
-/**
- *
- * @author myke
- */
 public class GoogleScholarAuthorAPIClientView {
     private GoogleScholarAuthorAPIClientController controller;
 
